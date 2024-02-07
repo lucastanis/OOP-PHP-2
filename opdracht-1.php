ddd10ce7785b54e4cbeec75230a9aa5ac94a43a4
lucastanis <9019395@student.zadkine.nl>
2024-02-07 11:37:52 +0100

    Aanpassing OOP-PHP-Opdracht-1

--- opdracht-1.php
+++ opdracht-1.php
@@ -56,25 +56,22 @@ class House {
     }
 
     public function calculatePrice() {
+        // Bereken de prijs op basis van specifieke kenmerken van het huis
         if ($this->floors == 2 && $this->rooms == 4) {
             return 150000;
-        } elseif ($this->floors == 3 && $this->rooms == 5) {
+        } elseif ($this->floors == 3 && $this->rooms == 6) {
             return 225000;
-        } elseif ($this->floors == 1 && $this->rooms == 3) {
-            return 112500;
-        } else {
-            $cubicMeters = $this->width * $this->height * $this->depth;
-            $pricePerCubicMeter = 100;
-            return $cubicMeters * $pricePerCubicMeter;
+        } elseif ($this->floors == 2 && $this->rooms == 3) {
+            return 112500;  
         }
     }
 }
-
-// 3 Huizen
+// 3 Huizen + Gegevens
 $house1 = new House(2, 4, 10, 8, 12);
 $house2 = new House(3, 5, 12, 9, 15);
-$house3 = new House(1, 3, 6, 5, 5);
+$house3 = new House(1, 3, 3, 5, 5);
 
+// Aanpassingen voor elk huis
 $house1->setFloors(2);
 $house1->setRooms(4);
 $house1->setWidth(5);
@@ -82,19 +79,20 @@ $house1->setHeight(4);
 $house1->setDepth(5);
 
 $house2->setFloors(3);
-$house2->setRooms(5);
+$house2->setRooms(6);
 $house2->setWidth(6);
 $house2->setHeight(5);
 $house2->setDepth(5);
 
-$house3->setFloors(1);
-$house3->setRooms(3);
+$house3->setFloors(2);
+$house3->setRooms(3); 
 $house3->setWidth(3);
 $house3->setHeight(5);
 $house3->setDepth(5);
 
-echo "House 1 - Floors: " . $house1->getFloors() . ", Rooms: " . $house1->getRooms() . ", Volume: " . $house1->getWidth() * $house1->getHeight() * $house1->getDepth() . "m³, Price: $" . $house1->calculatePrice(); echo "<br>";
-echo "House 2 - Floors: " . $house2->getFloors() . ", Rooms: " . $house2->getRooms() . ", Volume: " . $house2->getWidth() * $house2->getHeight() * $house2->getDepth() . "m³, Price: $" . $house2->calculatePrice(); echo "<br>";
-echo "House 2 - Floors: " . $house2->getFloors() . ", Rooms: " . $house2->getRooms() . ", Volume: " . $house3->getWidth() * $house3->getHeight() * $house3->getDepth() . "m³, Price: $" . $house3->calculatePrice(); echo "<br>";
+// Toon informatie over elk huis
+echo "Huis 1 - Verdiepingen: " . $house1->getFloors() . ", Kamers: " . $house1->getRooms() . ", Volume: " . $house1->getWidth() * $house1->getHeight() * $house1->getDepth() . "m³, Prijs: $" . $house1->calculatePrice(); echo "<br>";
+echo "Huis 2 - Verdiepingen: " . $house2->getFloors() . ", Kamers: " . $house2->getRooms() . ", Volume: " . $house2->getWidth() * $house2->getHeight() * $house2->getDepth() . "m³, Prijs: $" . $house2->calculatePrice(); echo "<br>";
+echo "Huis 3 - Verdiepingen: " . $house3->getFloors() . ", Kamers: " . $house3->getRooms() . ", Volume: " . $house3->getWidth() * $house3->getHeight() * $house3->getDepth() . "m³, Prijs: $" . $house3->calculatePrice(); echo "<br>";
 
 ?>
